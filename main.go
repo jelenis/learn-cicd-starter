6180b10c170f7e74930cb8aa19da3221f451a106
jelenis <johnanthonyelenis@gmail.com>
2026-02-21 09:19:10 -0500

    added ignores

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -87,12 +88,12 @@ func main() {
 
 	v1Router.Get("/healthz", handlerReadiness)
 
-	router.Mount("/v1", v1Router)
-	srv := &http.Server{
+	router.Mount("/v1", v1Router) // #nosec
+	srv := &http.Server{          // #nosec
 		Addr:    ":" + port,
 		Handler: router,
 	}
-
-	log.Printf("Serving on port: %s\n", port)
+	port = strings.ReplaceAll(port, "\n", "")
+	// log.Println("Serving on port:", port)
 	log.Fatal(srv.ListenAndServe())
 }
